95bbb4257b3ca7c78d954dacc5e9bbd9cbcac38d
sanjeev sharma <sanjeev.sharma@affluencesolutions.com>
2018-09-13 14:00:34 +0530

    passing property in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Propass from './Propass';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <Propass name="Sanjeev Kaushik" profile="Software Developer" /> 
+      </div>
+    );
+  }
+}
+
+export default App;
